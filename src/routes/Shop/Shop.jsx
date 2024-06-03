1ae9f7de7ef27dfd33855b62ea5de51e0d311a0d
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-06-03 11:19:24 +0600

    added dummy shop data json and showing the titles of the item in shop route

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import SHOP_DATA from "../../assets/data/shopData.json";
 
 const Shop = () => {
-  return <div>This is Shop</div>;
+  return (
+    <div>
+      {SHOP_DATA.map(({ id, name }) => (
+        <div key={id}>
+          <h1>{name}</h1>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Shop;
