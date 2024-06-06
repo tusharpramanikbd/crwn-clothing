63c9c2aa7d2c1182e9a97189b40fd3e157b350b0
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-06-06 11:54:09 +0600

    getting data from firestore and showing in the shop route

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,16 +1,23 @@
-import React, { useContext } from "react";
-import { ProductsContext } from "../../contexts/products.context";
+import React, { Fragment, useContext } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import "./Shop.styles.scss";
+import { CategoriesContext } from "../../contexts/categories.context";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+    <>
+      {Object.keys(categoriesMap).map((title) => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className="products-container">
+            {categoriesMap[title].map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
       ))}
-    </div>
+    </>
   );
 };
 
