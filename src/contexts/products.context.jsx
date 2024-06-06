37ff908f53df56aa31c730d6d8ca96095c984101
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-06-06 11:22:05 +0600

    added function to insert batch data to firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,13 @@
 import { createContext, useState } from "react";
-import PRODUCTS from "../assets/data/shopData.json";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = { products };
+
   return (
     <ProductsContext.Provider value={value}>
       {children}
