fa35a1aee1e36dd93ae14bd1ee1fa9bc33ccc744
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-07-16 10:48:13 +0600

    fixed sign up

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,54 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import InputField from "../InputField/InputField";
 import "./SignUp.styles.scss";
 import Button from "../Button/Button";
-import { useDispatch } from "react-redux";
-import { signUpStart } from "../../store/user/userAction";
-
-const defaultFormFields = {
-  displayName: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-};
+import { useSIgnUp } from "../../hooks/useSignUp";
 
 const SignUp = () => {
-  const dispatch = useDispatch();
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { displayName, email, password, confirmPassword } = formFields;
-
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    if (password !== confirmPassword) {
-      alert("Password do not match");
-      return;
-    }
-
-    try {
-      dispatch(signUpStart(email, password, displayName));
-      resetFormFields();
-    } catch (error) {
-      if (error.code === "auth/email-already-in-use") {
-        alert("Cannot create user, email already in use");
-      } else if (error.code === "auth/weak-password") {
-        alert("Password should be at least 6 characters");
-      } else {
-        console.log("User creation error: ", error);
-      }
-    }
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormFields({ ...formFields, [name]: value });
-  };
-
+  const {
+    handleSubmit,
+    handleChange,
+    displayName,
+    email,
+    password,
+    confirmPassword,
+  } = useSIgnUp();
   return (
     <div className="sign-up-container">
       <h2>Don't have an account?</h2>
---
+++ src/hooks/useSignUp.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebaseUtil";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+export const useSIgnUp = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Password do not match");
+      return;
+    }
+
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName });
+
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("Cannot create user, email already in use");
+      } else if (error.code === "auth/weak-password") {
+        alert("Password should be at least 6 characters");
+      } else {
+        console.log("User creation error: ", error);
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return {
+    handleSubmit,
+    handleChange,
+    displayName,
+    email,
+    password,
+    confirmPassword,
+  };
+};
