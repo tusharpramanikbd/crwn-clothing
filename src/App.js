b5c645c43e3fe4fd3525394933a4d45bd59372ad
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-07-28 19:51:43 +0600

    implemented dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import Home from "./routes/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/Navigation/Navigation";
-import Shop from "./routes/Shop/Shop";
-import Authentication from "./routes/Authentication/Authentication";
-import Checkout from "./routes/Checkout/Checkout";
 import { useCheckUserSession } from "./hooks/useCheckUserSession";
+import { lazy, Suspense } from "react";
+import Spinner from "./components/Spinner/Spinner";
+
+// Dynamic Imports
+const Home = lazy(() => import("./routes/Home/Home"));
+const Shop = lazy(() => import("./routes/Shop/Shop"));
+const Authentication = lazy(() =>
+  import("./routes/Authentication/Authentication")
+);
+const Checkout = lazy(() => import("./routes/Checkout/Checkout"));
 
 const App = () => {
   useCheckUserSession();
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="/shop/*" element={<Shop />} />
-        <Route path="/auth" element={<Authentication />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="/shop/*" element={<Shop />} />
+          <Route path="/auth" element={<Authentication />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
