18c60e4182d0974fae76b9b9f8c599c1765835cb
Tushar Pramanik <tusharpramanikbd@gmail.com>
2024-08-03 23:20:58 +0600

    added navigation component tests

--- src/__tests__/components/Navigation.test.tsx
+++ src/__tests__/components/Navigation.test.tsx
@@ -15,4 +15,49 @@ describe("Navigation component tests", () => {
     const signInLinkElement = screen.getByText(/sign in/i);
     expect(signInLinkElement).toBeInTheDocument();
   });
+
+  test("Verify that sign out is rendered if there is current user", () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        user: {
+          currentUser: {
+            createdAt: new Date(),
+            displayName: "test",
+            email: "test",
+          },
+        },
+      },
+    });
+
+    const signInLinkElement = screen.getByText(/sign out/i);
+    expect(signInLinkElement).toBeInTheDocument();
+  });
+
+  test("Verify that cart is not rendered if isCartOpen is false", () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: false,
+          cartItems: [],
+        },
+      },
+    });
+
+    const signInLinkElement = screen.queryByText(/your cart is empty/i);
+    expect(signInLinkElement).toBeNull();
+  });
+
+  test("Verify that cart is rendered if isCartOpen is true", () => {
+    renderWithProviders(<Navigation />, {
+      preloadedState: {
+        cart: {
+          isCartOpen: true,
+          cartItems: [],
+        },
+      },
+    });
+
+    const signInLinkElement = screen.queryByText(/your cart is empty/i);
+    expect(signInLinkElement).toBeInTheDocument();
+  });
 });
